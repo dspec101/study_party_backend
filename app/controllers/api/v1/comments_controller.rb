6b4476919e63c48552bc1f7ec5d13bb6b43a1646
David <davidspector08@gmail.com>
2020-09-30 15:04:40 -0400

    ready

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -19,16 +19,16 @@ class Api::V1::CommentsController < ApplicationController
     def create
         comment = Comment.create(comment_params)
         render json: comment
-        account_sid = 'AC148814fdf1fb0332fc6e26872b944a9f'
-        auth_token = 'ca1472db6a7875fded77865840e4cc0d'
-        client = Twilio::REST::Client.new(account_sid, auth_token)
-        from = '+14842287156' # Your Twilio number
-        to = '+19783176594' # Your mobile phone number
-        client.messages.create(
-        from: from,
-        to: to,
-        body: "Hey friend! Your study party room is hot! Someone just commented: #{comment.description} Join the learning!"
-        )
+        # account_sid = 'AC148814fdf1fb0332fc6e26872b944a9f'
+        # auth_token = 'ca1472db6a7875fded77865840e4cc0d'
+        # client = Twilio::REST::Client.new(account_sid, auth_token)
+        # from = '+14842287156' # Your Twilio number
+        # to = '+19783176594' # Your mobile phone number
+        # client.messages.create(
+        # from: from,
+        # to: to,
+        # body: "Hey friend! Your study party room is hot! Someone just commented: #{comment.description} Join the learning!"
+        # )
     end
 
     def destroy
